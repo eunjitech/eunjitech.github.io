a77578a44e2e2e12f71bc96e9f738acb8f0626a3
eunjitech <eunjitech@gmail.com>
2021-02-04 21:15:46 +0900

    하단오류수정 contact 완료

--- js/script.js
+++ js/script.js
@@ -9,25 +9,36 @@ $(function(){
     });
 });
 
-var tabList = document.querySelectorAll('.nav_menu li');
+const tabList = document.querySelectorAll('.nav_menu li');
 
 Array.prototype.forEach.call(tabList, function(list) {
     list.children[0].addEventListener('click', function(e) {
         e.preventDefault();
-        var tabContent = document.querySelectorAll('#content .content');
-        var tabNum = this.getAttribute('data-tabnum');
+        const tabContent = document.querySelectorAll('#content .content');
+        const tabNum = this.getAttribute('data-tabnum');
         
         Array.prototype.forEach.call(tabContent, function(cont, i) {
             cont.style.display = 'none';
             tabList[i].className = 'btn';
-        })
+        });
         tabContent[tabNum].style.display = 'block';
         
         if(list.className.indexOf('checked') == -1) {
-            list.className = 'btn checked'
+            list.className = 'btn checked';
         }
-    })
-})
+    });
+});
+
+const phoneText = document.querySelector('.contact .container h3');
+const phone = phoneText.querySelector('span');
+
+phoneText.addEventListener('mouseover', function(){
+    phone.classList.add('up');
+});
+
+phoneText.addEventListener('mouseleave', function(){
+    phone.classList.remove('up');
+});
 
 
 
